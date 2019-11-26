7efae0df93268505c8f32d791f7aab58eb3b4721
ISAIAH WILSON <ISAWILSO@uat.edu>
2019-11-26 13:18:41 -0700

    Multiple files

--- TextBasedRPG/TBRPG.cpp
+++ TextBasedRPG/TBRPG.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
- 
+#include "TBRPG.h"
 using namespace std;
 //creating a object class for the Human Race
 class human
@@ -74,354 +74,8 @@ public:
 	string Weapon;
 	int Damage = 0;
 };
-int up(int& vertical, int difficulty)
-{
-	//move up
-		if (vertical > 0)
-	{
-			vertical--;
-			
-	}
-		else
-		{
-			cout << "You are already at the top of the map\n\n";
-		}
-	return vertical;
-}
-int left(int& horizontal, int difficulty)
-{
-	//move left
-	if (horizontal > 0)
-	{
-		horizontal--;
-		
-	}
-	else
-	{
-		cout << "You are already at the left edge of the map\n\n";
-	}
-	return horizontal;
-}
-int right(int& horizontal, int difficulty)
-{
-	//move right
-	 if (difficulty == 1)
-	 {
-		 if (horizontal < 2)
-		 {
-			 horizontal++;
-			 cout << horizontal << "\n";
-		 }
-		 else
-		 {
-			 cout << "You are already at the right edge of the map\n\n";
-		 }
-	 }
-	 if (difficulty == 2)
-	 {
-		 if (horizontal < 4)
-		 {
-			 horizontal++;
-		 }
-		 else
-		 {
-			 cout << "You are already at the right edge of the map\n\n";
-		 }
-	 }
-	 if (difficulty == 3)
-	 {
-		 if (horizontal < 6)
-		 {
-			 horizontal++;
-		 }
-		 else
-		 {
-			 cout << "You are already at the right edge of the map\n\n";
-		 }
-	 }
-	 return horizontal;
-}
-int down(int& vertical, int difficulty)
-{
-	//move down
-	if (difficulty == 1)
-	{
-		if (vertical <= 2)
-		{
-			vertical++;
-			
-		}
-		else
-		{
-			cout << "You are already at the bottom of the map\n\n";
-		}
-	}
-	if (difficulty == 2)
-	{
-		if (vertical <= 4)
-		{
-			vertical++;
-		}
-		else
-		{
-			cout << "You are already at the bottom of the map\n\n";
-		}
-	}
-	if (difficulty == 3)
-	{
-		if (vertical <= 6)
-		{
-			vertical++;
-		}
-		else
-		{
-			cout << "You are already at the bottom of the map\n\n";
-		}
-	}
-	return vertical;
-}
-int monster(int& health, int& damage, int& difficulty, string& weapon, int& x, int& y, bool& boss, int& boss_x, int& boss_y, string& race)
-{
-	if (difficulty == 1)
-	{
-		string answer;
-		if (x != boss_x && y != boss_y)
-		{	
-			int chance = rand() % 4;
-			int monster_health = rand() % 5000 + 2000;
-			if (chance == 1)
-			{
-				cout << "A monster has come into your path.";
 
-				while (monster_health > 0)
-				{
-					cout << "Do you attack?\n yes/no\n";
-					cin >> answer;
-					if (answer == "yes")
-					{
-						cout << "You attacked with your " << weapon << " and did " << damage << " damage to the monster.\n";
-						monster_health -= damage;
-						if (monster_health <= 0)
-						{
-							cout << "You killed the monster, way to go\n";
-							if (race == "human")
-							{
-								human human;
-								human.HP += 50;
-							}
-							if (race == "dragoon")
-							{
-								dragoon dragoon;
-								dragoon.HP += 50;
-							}
-							if (race == "elf")
-							{
-								elf elf;
-								elf.HP += 50;
-							}
-							if (race == "pixie")
-							{
-								pixie pixie;
-								pixie.HP += 50;
-							}
-						}
-						else
-						{
-							cout << "The monster counter attacked and dealt 10 points of damage.\n";
-							health = health - 10;
-							cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
-						}
-					}
-					else if (answer == "no")
-					{
-						cout << "You did not attack.\n";
-						cout << "However the monster still attacked you dealing 10 points of damage.\n";
-						health = health - 10;
-						cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
-					}
-				}
-			}
-			else
-			{
-				cout << "No monster appeared before you.\n";
-			}
-		}
-		else if (x == 2 && y == 2)
-		{
-			int monster_health = 10000;
-			cout << "!!You have found the Great Serpent Gregory!!\n";
-			while (monster_health > 0)
-			{
-				cout << "Will you attack? \nyes/no\n";
-				cin >> answer;
 
-				if (answer == "yes")
-				{
-					cout << "You attacked with your " << weapon << " and did " << damage << " damage to the monster.\n";
-					monster_health -= damage;
-					if (monster_health <= 0)
-					{
-						cout << "You killed the monster, way to go\n";
-						cout << "You cut out his heart so you can take it back to the king.\n";
-						boss = true;
-					}
-					else
-					{
-
-						cout << "The monster counter attacked and dealt 10 points of damage.\n";
-						health = health - 20;
-						cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
-					}
-				}
-				else if (answer == "no")
-				{
-					cout << "You did not attack.\n";
-					cout << "However the monster still attacked you dealing 20 points of damage.\n";
-					health = health - 20;
-					cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
-				}
-			}
-		}
-	}
-	if (difficulty == 2)
-	{
-		int chance = rand() % 2;
-		int monster_health = rand() % 10000 + 5000;
-		string answer;
-		if (x != boss_x && y != boss_y)
-		{
-			if (chance == 1)
-			{
-				cout << "A monster has come into your path.";
-				while (monster_health > 0)
-				{
-					cout << "Do you attack?\n yes/no\n";
-					cin >> answer;
-					if (answer == "yes")
-					{
-						cout << "You attacked with your " << weapon << " and did " << damage << " damage to the monster.\n";
-						monster_health -= damage;
-						if (monster_health == 0)
-						{
-							cout << "You killed the monster, way to go\n";
-							if (race == "human")
-							{
-								human human;
-								human.HP += 100;
-							}
-							if (race == "dragoon")
-							{
-								dragoon dragoon;
-								dragoon.HP += 100;
-							}
-							if (race == "elf")
-							{
-								elf elf;
-								elf.HP += 100;
-							}
-							if (race == "pixie")
-							{
-								pixie pixie;
-								pixie.HP += 100;
-							}
-						}
-						else
-						{
-							cout << "The monster counter attacked and dealt 50 points of damage.\n";
-							health = health - 50;
-							cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
-						}
-					}
-					else if (answer == "no")
-					{
-						cout << "You did not attack.\n";
-						cout << "However the monster still attacked you dealing 50 points of damage.\n";
-						health = health - 50;
-						
-					}
-				}
-			}
-			else
-			{
-				cout << "No monster appeared before you.\n";
-			}
-		}
-		if (x == boss_x && y == boss_y)
-		{
-			//create Karen
-			 monster_health = 30000;
-			cout << "!!You have found Karen, The Kraken!!\n";
-			while (monster_health > 0)
-			{
-				cout << "Will you attack? \nyes/no\n";
-				cin >> answer;
-
-				if (answer == "yes")
-				{
-					cout << "You attacked with your " << weapon << " and did " << damage << " damage to the monster.\n";
-					monster_health -= damage;
-					if (monster_health <= 0)
-					{
-						cout << "You killed the monster, way to go\n";
-						cout << "You cut out his heart so you can take it back to the king.\n";
-						boss = true;
-					}
-					else
-					{
-
-						cout << "The monster counter attacked and dealt 10 points of damage.\n";
-						health = health - 75;
-						cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
-					}
-				}
-				else if (answer == "no")
-				{
-					cout << "You did not attack.\n";
-					cout << "However the monster still attacked you dealing 20 points of damage.\n";
-					health = health - 75;
-					cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
-				}
-			}
-		}
-	}
-	if (difficulty == 3)
-	{
-		int chance = 1;
-		int monster_health = rand() % 20000 + 10000;
-		string answer;
-		if (chance == 1)
-		{
-			cout << "A monster has come into your path.";
-			cout << "Do you attack?\n yes/no\n";
-			cin >> answer;
-			if (answer == "yes")
-			{
-				cout << "You attacked with your " << weapon << " and did " << damage << " damage to the monster.\n";
-				monster_health -= damage;
-				if (monster_health <= 0)
-				{
-					cout << "You killed the monster, way to go";
-				}
-				else 
-				{
-					cout << "The monster counter attacked and dealt 100 points of damage.\n";
-					health = health - 100;
-				}
-			}
-			else if (answer == "no")
-			{
-				cout << "You did not attack.\n";
-				cout << "However the monster still attacked you dealing 75 points of damage.\n";
-				health = health - 75;
-			}
-		}
-		else
-		{
-			cout << "No monster appeared before you.\n";
-		}
-	}
-	return health;
-}
 int main()
 {
 	bool playAgain = false;
@@ -606,21 +260,101 @@ int main()
 					{
 						up(y,difficulty);
 						monster(health, damage, difficulty, player.Weapon,x,y,boss,gregory_x,gregory_y,player.Race);
+						if (race == "human")
+						{
+							human human;
+							human.HP += 50;
+						}
+						if (race == "dragoon")
+						{
+							dragoon dragoon;
+							dragoon.HP += 50;
+						}
+						if (race == "elf")
+						{
+							elf elf;
+							elf.HP += 50;
+						}
+						if (race == "pixie")
+						{
+							pixie pixie;
+							pixie.HP += 50;
+						}
 					}
 					if (movement == "down")
 					{
 						down(y, difficulty);
 						monster(health, damage, difficulty, player.Weapon,x,y,boss, gregory_x, gregory_y,player.Race);
+						if (race == "human")
+						{
+							human human;
+							human.HP += 50;
+						}
+						if (race == "dragoon")
+						{
+							dragoon dragoon;
+							dragoon.HP += 50;
+						}
+						if (race == "elf")
+						{
+							elf elf;
+							elf.HP += 50;
+						}
+						if (race == "pixie")
+						{
+							pixie pixie;
+							pixie.HP += 50;
+						}
 					}
 					if (movement == "left")
 					{
 						left(x, difficulty);
 						monster(health, damage, difficulty, player.Weapon,x,y,boss, gregory_x, gregory_y,player.Race);
+						if (race == "human")
+						{
+							human human;
+							human.HP += 50;
+						}
+						if (race == "dragoon")
+						{
+							dragoon dragoon;
+							dragoon.HP += 50;
+						}
+						if (race == "elf")
+						{
+							elf elf;
+							elf.HP += 50;
+						}
+						if (race == "pixie")
+						{
+							pixie pixie;
+							pixie.HP += 50;
+						}
 					}
 					if (movement == "right")
 					{
 						right(x, difficulty);
 						monster(health, damage, difficulty, player.Weapon, x ,y,boss, gregory_x, gregory_y,player.Race);
+						if (race == "human")
+						{
+							human human;
+							human.HP += 50;
+						}
+						if (race == "dragoon")
+						{
+							dragoon dragoon;
+							dragoon.HP += 50;
+						}
+						if (race == "elf")
+						{
+							elf elf;
+							elf.HP += 50;
+						}
+						if (race == "pixie")
+						{
+							pixie pixie;
+							pixie.HP += 50;
+						}
 					}
 				}
 				if (boss == true)
@@ -770,26 +504,102 @@ int main()
 					if (movement == "up")
 					{
 						up(y, difficulty);
-						cout << y << "\n";
 						monster(health, damage, difficulty, player.Weapon, x, y, boss, boss_x, boss_y,player.Race);
+						if (race == "human")
+						{
+							human human;
+							human.HP += 50;
+						}
+						if (race == "dragoon")
+						{
+							dragoon dragoon;
+							dragoon.HP += 50;
+						}
+						if (race == "elf")
+						{
+							elf elf;
+							elf.HP += 50;
+						}
+						if (race == "pixie")
+						{
+							pixie pixie;
+							pixie.HP += 50;
+						}
 					}
 					if (movement == "down")
 					{
 						down(y, difficulty);
-						cout << y << "\n";
 						monster(health, damage, difficulty, player.Weapon, x, y, boss, boss_x, boss_y,player.Race);
+						if (race == "human")
+						{
+							human human;
+							human.HP += 50;
+						}
+						if (race == "dragoon")
+						{
+							dragoon dragoon;
+							dragoon.HP += 50;
+						}
+						if (race == "elf")
+						{
+							elf elf;
+							elf.HP += 50;
+						}
+						if (race == "pixie")
+						{
+							pixie pixie;
+							pixie.HP += 50;
+						}
 					}
 					if (movement == "left")
 					{
 						left(x, difficulty);
-						cout << x << "\n";
 						monster(health, damage, difficulty, player.Weapon, x, y, boss, boss_x, boss_y,player.Race);
+						if (race == "human")
+						{
+							human human;
+							human.HP += 50;
+						}
+						if (race == "dragoon")
+						{
+							dragoon dragoon;
+							dragoon.HP += 50;
+						}
+						if (race == "elf")
+						{
+							elf elf;
+							elf.HP += 50;
+						}
+						if (race == "pixie")
+						{
+							pixie pixie;
+							pixie.HP += 50;
+						}
 					}
 					if (movement == "right")
 					{
 						right(x, difficulty);
-						cout << x << "\n";
 						monster(health, damage, difficulty, player.Weapon, x, y, boss, boss_x, boss_y,player.Race);
+						if (race == "human")
+						{
+							human human;
+							human.HP += 50;
+						}
+						if (race == "dragoon")
+						{
+							dragoon dragoon;
+							dragoon.HP += 50;
+						}
+						if (race == "elf")
+						{
+							elf elf;
+							elf.HP += 50;
+						}
+						if (race == "pixie")
+						{
+							pixie pixie;
+							pixie.HP += 50;
+						}
 					}
 				}
 				if (boss == true)
---
+++ TextBasedRPG/TBRPG.h
@@ -0,0 +1,313 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+int up(int& vertical, int difficulty)
+{
+	//move up
+	if (vertical > 0)
+	{
+		vertical--;
+
+	}
+	else
+	{
+		cout << "You are already at the top of the map\n\n";
+	}
+	return vertical;
+}
+int left(int& horizontal, int difficulty)
+{
+	//move left
+	if (horizontal > 0)
+	{
+		horizontal--;
+
+	}
+	else
+	{
+		cout << "You are already at the left edge of the map\n\n";
+	}
+	return horizontal;
+}
+int right(int& horizontal, int difficulty)
+{
+	//move right
+	if (difficulty == 1)
+	{
+		if (horizontal < 2)
+		{
+			horizontal++;
+			cout << horizontal << "\n";
+		}
+		else
+		{
+			cout << "You are already at the right edge of the map\n\n";
+		}
+	}
+	if (difficulty == 2)
+	{
+		if (horizontal < 4)
+		{
+			horizontal++;
+		}
+		else
+		{
+			cout << "You are already at the right edge of the map\n\n";
+		}
+	}
+	if (difficulty == 3)
+	{
+		if (horizontal < 6)
+		{
+			horizontal++;
+		}
+		else
+		{
+			cout << "You are already at the right edge of the map\n\n";
+		}
+	}
+	return horizontal;
+}
+int down(int& vertical, int difficulty)
+{
+	//move down
+	if (difficulty == 1)
+	{
+		if (vertical <= 2)
+		{
+			vertical++;
+
+		}
+		else
+		{
+			cout << "You are already at the bottom of the map\n\n";
+		}
+	}
+	if (difficulty == 2)
+	{
+		if (vertical <= 4)
+		{
+			vertical++;
+		}
+		else
+		{
+			cout << "You are already at the bottom of the map\n\n";
+		}
+	}
+	if (difficulty == 3)
+	{
+		if (vertical <= 6)
+		{
+			vertical++;
+		}
+		else
+		{
+			cout << "You are already at the bottom of the map\n\n";
+		}
+	}
+	return vertical;
+}
+int monster(int& health, int& damage, int& difficulty, string& weapon, int& x, int& y, bool& boss, int& boss_x, int& boss_y, string& race)
+{
+	if (difficulty == 1)
+	{
+		string answer;
+		if (x != boss_x && y != boss_y)
+		{
+			int chance = rand() % 4;
+			int monster_health = rand() % 5000 + 2000;
+			if (chance == 1)
+			{
+				cout << "A monster has come into your path.";
+
+				while (monster_health > 0)
+				{
+					cout << "Do you attack?\n yes/no\n";
+					cin >> answer;
+					if (answer == "yes")
+					{
+						cout << "You attacked with your " << weapon << " and did " << damage << " damage to the monster.\n";
+						monster_health -= damage;
+						if (monster_health <= 0)
+						{
+							cout << "You killed the monster, way to go\n";
+							
+						}
+						else
+						{
+							cout << "The monster counter attacked and dealt 10 points of damage.\n";
+							health = health - 10;
+							cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
+						}
+					}
+					else if (answer == "no")
+					{
+						cout << "You did not attack.\n";
+						cout << "However the monster still attacked you dealing 10 points of damage.\n";
+						health = health - 10;
+						cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
+					}
+				}
+			}
+			else
+			{
+				cout << "No monster appeared before you.\n";
+			}
+		}
+		else if (x == 2 && y == 2)
+		{
+			int monster_health = 10000;
+			cout << "!!You have found the Great Serpent Gregory!!\n";
+			while (monster_health > 0)
+			{
+				cout << "Will you attack? \nyes/no\n";
+				cin >> answer;
+
+				if (answer == "yes")
+				{
+					cout << "You attacked with your " << weapon << " and did " << damage << " damage to the monster.\n";
+					monster_health -= damage;
+					if (monster_health <= 0)
+					{
+						cout << "You killed the monster, way to go\n";
+						cout << "You cut out his heart so you can take it back to the king.\n";
+						boss = true;
+					}
+					else
+					{
+
+						cout << "The monster counter attacked and dealt 10 points of damage.\n";
+						health = health - 20;
+						cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
+					}
+				}
+				else if (answer == "no")
+				{
+					cout << "You did not attack.\n";
+					cout << "However the monster still attacked you dealing 20 points of damage.\n";
+					health = health - 20;
+					cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
+				}
+			}
+		}
+	}
+	if (difficulty == 2)
+	{
+		int chance = rand() % 2;
+		int monster_health = rand() % 10000 + 5000;
+		string answer;
+		if (x != boss_x && y != boss_y)
+		{
+			if (chance == 1)
+			{
+				cout << "A monster has come into your path.";
+				while (monster_health > 0)
+				{
+					cout << "Do you attack?\n yes/no\n";
+					cin >> answer;
+					if (answer == "yes")
+					{
+						cout << "You attacked with your " << weapon << " and did " << damage << " damage to the monster.\n";
+						monster_health -= damage;
+						if (monster_health == 0)
+						{
+							cout << "You killed the monster, way to go\n";
+						}
+						else
+						{
+							cout << "The monster counter attacked and dealt 50 points of damage.\n";
+							health = health - 50;
+							cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
+						}
+					}
+					else if (answer == "no")
+					{
+						cout << "You did not attack.\n";
+						cout << "However the monster still attacked you dealing 50 points of damage.\n";
+						health = health - 50;
+
+					}
+				}
+			}
+			else
+			{
+				cout << "No monster appeared before you.\n";
+			}
+		}
+		if (x == boss_x && y == boss_y)
+		{
+			//create Karen
+			monster_health = 30000;
+			cout << "!!You have found Karen, The Kraken!!\n";
+			while (monster_health > 0)
+			{
+				cout << "Will you attack? \nyes/no\n";
+				cin >> answer;
+
+				if (answer == "yes")
+				{
+					cout << "You attacked with your " << weapon << " and did " << damage << " damage to the monster.\n";
+					monster_health -= damage;
+					if (monster_health <= 0)
+					{
+						cout << "You killed the monster, way to go\n";
+						cout << "You cut out his heart so you can take it back to the king.\n";
+						boss = true;
+					}
+					else
+					{
+
+						cout << "The monster counter attacked and dealt 10 points of damage.\n";
+						health = health - 75;
+						cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
+					}
+				}
+				else if (answer == "no")
+				{
+					cout << "You did not attack.\n";
+					cout << "However the monster still attacked you dealing 20 points of damage.\n";
+					health = health - 75;
+					cout << "Your teams health: " << health << "\nThe monsters health: " << monster_health << "\n";
+				}
+			}
+		}
+	}
+	if (difficulty == 3)
+	{
+		int chance = 1;
+		int monster_health = rand() % 20000 + 10000;
+		string answer;
+		if (chance == 1)
+		{
+			cout << "A monster has come into your path.";
+			cout << "Do you attack?\n yes/no\n";
+			cin >> answer;
+			if (answer == "yes")
+			{
+				cout << "You attacked with your " << weapon << " and did " << damage << " damage to the monster.\n";
+				monster_health -= damage;
+				if (monster_health <= 0)
+				{
+					cout << "You killed the monster, way to go";
+				}
+				else
+				{
+					cout << "The monster counter attacked and dealt 100 points of damage.\n";
+					health = health - 100;
+				}
+			}
+			else if (answer == "no")
+			{
+				cout << "You did not attack.\n";
+				cout << "However the monster still attacked you dealing 75 points of damage.\n";
+				health = health - 75;
+			}
+		}
+		else
+		{
+			cout << "No monster appeared before you.\n";
+		}
+	}
+	return health;
+}
